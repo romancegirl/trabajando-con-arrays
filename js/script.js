9ed75df02b65dd07291613275802c515f281859f
Barbara Bassin <53198802+romancegirl@users.noreply.github.com>
2024-10-06 23:10:28 -0300

    JS Update

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Filtra los elementos que son de tipo string
+  const stringElements = strangeArray.filter((element) => typeof element === "string");
+
+  // Ordena los elementos de tipo string alfabéticamente, ignorando mayúsculas/minúsculas
+  const sortedStrings = stringElements.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+  showList(sortedStrings);
+});
